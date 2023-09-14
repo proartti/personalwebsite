4833e3be14aa5028886e17cce194983bc2d93217
Tiago Donatti <tdonatti777@gmail.com>
2023-09-13 19:05:54 -0700

    added custom colors and font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				purple: 'rgb(var(--purple) / <alpha-value>)',
+				'space-purple': 'rgb(var(--space-purple) / <alpha-value>)',
+				'blue-grey': 'rgb(var(--blue-grey) / <alpha-value>)',
+				'mid-grey': 'rgb(var(--mid-grey) / <alpha-value>)',
+				'gold-yellow': 'rgb(var(--gold-yellow) / <alpha-value>)',
+				'section-title': 'rgb(var(--section-title) / <alpha-value>)',
+				'section-title-small': 'rgb(var(--section-title-small) / <alpha-value>)',
+				'section-subtitle': 'rgb(var(--section-subtitle) / <alpha-value>)',
+			},
+			fontFamily: {
+				industry: ['industry-inc-base', 'sans-serif'],
+				roboto: ['roboto', 'sans-serif'],
+				'roboto-condensed': ['roboto-condensed', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
